b4194a3002933162a96670c6d768501e58bebff7
GwonDalhyeon <pygmalion85@gmail.com>
2013-11-12 15:43:12 +0900

    Recover code
    Recover code

--- Collinocorner.m
+++ Collinocorner.m
@@ -14,7 +14,7 @@ alpha = zeros(L,1);
 beta = zeros(L,1);
 C = zeros(L);
 A = zeros(L,1);
-u = zeros(Nx+1,Ny+1,Nt+1);
+u = zeros(Nx+1,Ny+1,Nt+1);5
 angle = zeros(L,1);
 M =zeros(2*L,2*L); % matrix corresponding to linear system for obtaining auxiliary function on the semi-boundary node
 P1 = zeros(L) ; 
@@ -33,14 +33,16 @@ beta =  2/(2*L+1) * (sin(angle)).^2;
 % u(x,y,t)=f(x,y,t)=0 for (x,y) on the boundary : boundary condition
 % u(x,y,0) = sin(pi*x)*sin(2*pi*y), u_t(x,y,0) = 0 : boundary condition
 syms x y t k1 k2 w
-f = @(x,y,t) 0
-g = fourier(fourier(f,x,k1),t,w)
-
+f = @(x,y,t) exp(-x^2-y^2-t^2);
+g = fourier(fourier(f,x,k1),t,w);
+psi_cell = cell(4,L);
 for l=1:5
-
-    % eval(['phi1_temp', num2str(l), ' = @(k1,w)',char(g)]);
-     array{i} = {eval(['phi2_temp', num2str(l), '=@(k2,w)', char(g)])};
-
+    
+%     eval(['phi1_temp', num2str(l), ' = @(k1,w)',char(g)]);
+%     psi_cell{1,l} = eval(['phi2_temp', num2str(l), '=@(k2,w)', char(g)]);
+    eval(['phi2_temp', num2str(l), '=@(k2,w)', char(g)]);
+    psi_cell{1,l} = eval(['phi2_temp', num2str(l)]);
+    
 end
     
 
